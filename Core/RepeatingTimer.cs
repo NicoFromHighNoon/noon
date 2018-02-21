845dc126eb66cbbf5970e27239331480789b0ca2
NicoFromHighNoon <36701992+NicoFromHighNoon@users.noreply.github.com>
2018-02-22 00:31:16 +1000

    Add files via upload

---
+++ Core/RepeatingTimer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Discord;
+using Discord.WebSocket;
+
+namespace nicobot.Core
+{
+    internal static class RepeatingTimer
+    {
+        private static Timer loopingTimer;
+        private static SocketTextChannel channel;
+
+        internal static Task StartTimer()
+        {
+            channel = Global.Client.GetGuild(388428205763198986).GetTextChannel(411979495583514635);
+
+            loopingTimer = new Timer()
+            {
+                Interval = 600000,
+                AutoReset = true,
+                Enabled = true
+            };
+            loopingTimer.Elapsed += OnTimerTicked;
+
+            return Task.CompletedTask;
+        }
+
+        private static async void OnTimerTicked(object sender, ElapsedEventArgs e)
+        {
+            string Umu1 = "https://cdn.discordapp.com/attachments/406605927945601034/415383154845810690/Umuanatomytriggermentionlistbewbdudestriggermentionlistthiccthighstriggermentionlistassmenifyoure_5a.jpg";
+            string Umu2 = "https://cdn.discordapp.com/attachments/406605927945601034/415382561989197825/dwM2GOrtjyBf4BLvNQuTqdxcDo1V8D-EalojKruk3W8.jpg";
+            string Umu3 = "https://cdn.discordapp.com/attachments/406605927945601034/415382280526233600/DPsqa8uW4AATNRA.jpg";
+            string Umu4 = "https://cdn.discordapp.com/attachments/406605927945601034/415382275480616960/DKa3nuSXUAE4hdJ.jpg";
+            string Umu5 = "https://cdn.discordapp.com/attachments/406605927945601034/415382272917766154/Dailydoseofumutriggermentionlistsmolholtriggermentionlistlewds4dheartsmolholhas_f5c8cc_6387606.jpg";
+            string Umu6 = "https://cdn.discordapp.com/attachments/406605927945601034/415382267440136214/Blazedumutriggermentionlistsmolholtriggermentionlistlewds4dheart_d627bf_6405778.jpg";
+            string Umu7 = "https://cdn.discordapp.com/attachments/406605927945601034/415382264344608768/Blank_cd8105494089d8c4b480fc6d30fc505d.jpg";
+            string Umu8 = "https://cdn.discordapp.com/attachments/406605927945601034/415382260263550977/aPjWewG_460s.jpg";
+            string Umu9 = "https://cdn.discordapp.com/attachments/406605927945601034/415382256522100736/396567947541217280.gif";
+            string Umu10 = "https://cdn.discordapp.com/attachments/406605927945601034/415382253728694273/18157204_10155647518255348_4256301199928973229_n.png";
+            string Umu11 = "https://cdn.discordapp.com/attachments/406605927945601034/415382245017124864/13732258_233031913764106_919167750_n.jpg";
+            string Umu12 = "https://cdn.discordapp.com/attachments/406605927945601034/415382244748951563/Umutriggermentionlistlewds4dhearttriggermentionlistsmolholyouhadonejobfj_383a9b_6400735.jpg";
+            string Umu13 = "https://cdn.discordapp.com/attachments/406605927945601034/415382177472053248/Umu_0ec32b17784865a4d31a53eb60092a60.jpg";
+            string Umu14 = "https://cdn.discordapp.com/attachments/406605927945601034/415382175358124035/tumblr_p3r0ky04Jf1vssigio3_500.jpg";
+            string Umu15 = "https://cdn.discordapp.com/attachments/406605927945601034/415382169448349711/tumblr_owzo761kCk1vwgwh5o1_500.jpg";
+            string Umu16 = "https://cdn.discordapp.com/attachments/406605927945601034/415382168047583233/tumblr_ovze19kh6E1uemqfco1_1280.png";
+            string Umu17 = "https://cdn.discordapp.com/attachments/406605927945601034/415382166894149632/tumblr_ool74z8Pwf1ssg673o2_500.png";
+            string Umu18 = "https://cdn.discordapp.com/attachments/406605927945601034/415382162989383681/tumblr_om45qqTbnz1rf6fvpo1_500.jpg";
+            string Umu19 = "https://cdn.discordapp.com/attachments/406605927945601034/415382162087608320/qr8jbc1y8gsy.jpg";
+            string Umu20 = "https://cdn.discordapp.com/attachments/406605927945601034/415382161399480321/tenor.gif";
+            string Umu21 = "https://cdn.discordapp.com/attachments/406605927945601034/415382160996827153/suZ2i60.png";
+            string Umu22 = "https://cdn.discordapp.com/attachments/406605927945601034/415382158434238464/mods_faceplates_nero.jpg";
+
+            string images = $"{Umu1} или {Umu2} или {Umu3} или {Umu4} или {Umu5} или {Umu6} или {Umu7} или {Umu8} или {Umu9} или {Umu10} или {Umu11} или {Umu12} или {Umu13} или {Umu14} или {Umu15} или {Umu16} или {Umu17} или {Umu18} или {Umu19} или {Umu20} или {Umu21} или {Umu22}";
+            string[] options = images.Split(new string[] { "или" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string selection = options[r.Next(0, options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Новая порция Уму!");
+            embed.WithImageUrl(selection);
+            embed.WithColor(new Color(240, 160, 30));
+
+            await channel.SendMessageAsync("", false, embed);
+        }
+    }
+}
---
+++ Modules/Misc.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+using nicobot.Core.UserAccounts;
+using NReco.ImageGenerator;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace nicobot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("WhatLevelIs")]
+        public async Task WhatLevelIs(uint xp)
+        {
+            uint level = (uint)Math.Sqrt(xp / 50);
+            await Context.Channel.SendMessageAsync("The level is " + level);
+        }
+
+        [Command("react")]
+        public async Task HandleReactionMessage()
+        {
+            RestUserMessage msg = await Context.Channel.SendMessageAsync("react to me");
+            Global.MessageIdToTrack = msg.Id;
+        }
+
+        [Command("stats")]
+        public async Task Stats([Remainder]string arg = "")
+        {
+            SocketUser target = null;
+            var mentionedUser = Context.Message.MentionedUsers.FirstOrDefault();
+            target = mentionedUser ?? Context.User;
+
+            var account = UserAccounts.GetAccount(target);
+            await Context.Channel.SendMessageAsync($"{target.Mention} has {account.XP} exp and {account.Points} points.");
+        }
+
+        [Command("hello")]
+        public async Task Hello([Remainder] string arg = "")
+        {
+            var mentionedUser = Context.Message.MentionedUsers.FirstOrDefault();
+            string css = "<style>\n    body{\n        background-image: url(https://cdn.discordapp.com/attachments/393526436343971851/415031891268206593/Nero-text.jpg);\n    }\n    h1{\n        color: rgb(180, 0, 0)\n    }\n</style>\n";
+            string html = String.Format("<font size=20><h1>Welcome to Aestus, {0}</h1></font>", mentionedUser.Username);
+            var converter = new HtmlToImageConverter
+            {           
+                Width = 2056,
+                Height = 1156
+            };
+            var pngBytes = converter.GenerateImage(css + html, NReco.ImageGenerator.ImageFormat.Jpeg);
+            await Context.Channel.SendFileAsync(new MemoryStream(pngBytes), "слыш письку саси :))).jpg");
+        }
+
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Ban()
+        {
+
+        }
+
+        [Command("addXP")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task AddXP(uint xp)
+        {
+            var account = UserAccounts.GetAccount(Context.User);
+            account.XP += xp;
+            UserAccounts.SaveAccounts();
+            await Context.Channel.SendMessageAsync($"You gained {xp} xp.");
+        }
+
+        [Command("help")]
+        public async Task Help()
+        {
+            await Context.Channel.SendMessageAsync("ну типа тут нихуя пока нет)0) \nбота заного делаю<:Xo4y_CDoXHyT_Blyat:403812608354025472>");
+        }
+
+        [Command("say")]
+        public async Task Say([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Пидор на " + Context.User.Username + " сказал");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 180, 0));
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+
+        [Command("choose")]
+        public async Task Pickone([Remainder]string message)
+        {
+            string[] options = message.Split(new string[] { "или" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string selection = options[r.Next(0, options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("трунь");
+            embed.WithDescription(selection);
+            embed.WithColor(new Color(255, 255, 0));
+            embed.WithThumbnailUrl("https://cdn.discordapp.com/emojis/403898766727577611.png?v=1");
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+            DataStorage.AddPairToStorage(Context.User.Username + " " + DateTime.Today + DateTime.Now.ToLongTimeString(), selection);
+
+        }
+
+        [Command("welcome")]
+        public async Task Welcome()
+        {
+            string user = Context.User.Username;
+            string guild = "Aestus Domus Aurea";
+            string botName = "Неро";
+            string message = String.Format("Привет, **{0}**. Я бот гильдии **{1}**. Меня зовут 🌹**{2}**🌹. Могу ли я помочь тебе?", user, guild, botName);
+
+            await Context.Channel.SendMessageAsync(message);
+        }
+
+        [Command("secret")]
+        public async Task RevSecret([Remainder]string arg = "")
+        {
+            if (!UserIsSecretOwner((SocketGuildUser)Context.User))
+            {
+                var embed = new EmbedBuilder();
+                embed.WithDescription("получи pepsi, " + Context.User.Mention);
+                embed.WithColor(new Color(0xFFFFFF));
+
+                await Context.Channel.SendMessageAsync("", false, embed);
+                return;
+            }
+            var dmChannel = await Context.User.GetOrCreateDMChannelAsync();
+            await dmChannel.SendMessageAsync(Utilities.GetAlert("SECRET"));
+        }
+
+        private bool UserIsSecretOwner(SocketGuildUser user)
+        {
+            string targetRoleName = "bepis";
+            var result = from r in user.Guild.Roles
+                         where r.Name == targetRoleName
+                         select r.Id;
+            ulong roleID = result.FirstOrDefault();
+            if (roleID == 0) return false;
+            var targetRole = user.Guild.GetRole(roleID);
+            return user.Roles.Contains(targetRole);
+        }
+        
+        [Command("data")]
+        public async Task GetData()
+        {
+            await Context.Channel.SendMessageAsync("Data Has " + DataStorage.GetPairsCount() + " pairs.");
+            DataStorage.AddPairToStorage("Count" + DataStorage.GetPairsCount(), "TheCount" + DataStorage.GetPairsCount());
+        }
+    }
+}
